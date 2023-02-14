137317e07cfb489e0329528a5c09181836bd1a4b
Aniket Kumar <90708238+AniketKumar2500@users.noreply.github.com>
2023-02-14 19:00:07 +0530

    Update Reference_Counter.h

--- Reference_Counter.h
+++ Reference_Counter.h
@@ -1,41 +1,69 @@
-#if !defined(Reference_Counter_H)
-#define Reference_Counter_H
+#ifndef REFERENCECOUNT_H_CSQ4
+#define REFERENCECOUNT_H_CSQ4
+#include <memory>
+
 template <typename T>
-class SmartPointer {
- public:
-  SmartPointer(T* ptr) : ptr_(ptr), ref_count_(new int(1)) {}
+class ref : std::shared_ptr<T> {
+public:
+    ref() : std::shared_ptr<T>(nullptr) {}
+    inline ref(T *ptr) : std::shared_ptr<T>(ptr) {}
+    inline ref(T val) : std::shared_ptr<T>(new T(val)) {}
+    inline ref(std::shared_ptr<T> ptr) : std::shared_ptr<T>(ptr) {}
+    inline ref(const ref<T> &sp) : std::shared_ptr<T>(sp) {}
 
-  SmartPointer(const SmartPointer<T>& other) :
-      ptr_(other.ptr_), ref_count_(other.ref_count_) {
-    ++(*ref_count_);
-  }
-
-  SmartPointer<T>& operator=(const SmartPointer<T>& other) {
-    if (this != &other) {
-      if (--(*ref_count_) == 0) {
-        delete ptr_;
-        delete ref_count_;
-      }
-      ptr_ = other.ptr_;
-      ref_count_ = other.ref_count_;
-      ++(*ref_count_);
+    inline ref<T> &operator=(const ref<T> &sp) {
+        this->std::shared_ptr<T>::operator=(sp);
+        return *this;
     }
-    return *this;
-  }
 
-  ~SmartPointer() {
-    if (--(*ref_count_) == 0) {
-      delete ptr_;
-      delete ref_count_;
+    inline T *operator->() const {
+        return this->get();
     }
-  }
 
-  T& operator*() const { return *ptr_; }
-  T* operator->() const { return ptr_; }
+    inline T &operator*() const {
+        return *(this->get());
+    }
 
- private:
-  T* ptr_;
-  int* ref_count_;
+    inline void reset(T *ptr) {
+        this->reset(ptr);
+    }
+    inline ref<T> operator++(int){
+        return ref<T>(T(this->get()+(1)));
+    }
+    inline ref<T> operator+(const ref<T> &rhs) {
+        return ref<T>(new T(this->get()->op_add(*this->get(),rhs.get())));
+    }
+
+
+    inline ref<T> operator-(const ref<T> &rhs) {
+        return ref<T>(new T(this->get()->op_sub(*this->get(),rhs.get())));
+    }
+    inline ref<T> operator*(const ref<T> &rhs) {
+        return ref<T>(new T(this->get()->op_mul(*this->get(),rhs.get())));
+    }
+    inline ref<T> operator/(const ref<T> &rhs) {
+        return ref<T>(new T(this->get()->op_div(*this->get(),rhs.get())));
+    }
+    inline bool operator>(const ref<T> &rhs) {
+        return ((this->get()->op_greater(*this->get(),rhs.get())));
+    }
+    inline bool operator<(const ref<T> &rhs) {
+        return ((this->get()->op_less(*this->get(),rhs.get())));
+    }
+    inline bool operator>=(const ref<T> &rhs) {
+        return ((this->get()->op_greaterEqual(*this->get(),rhs.get())));
+    }
+    inline bool operator<=(const ref<T> &rhs) {
+        return ((this->get()->op_lessEqual(*this->get(),rhs.get())));
+    }
+    inline bool operator!=(const ref<T> &rhs) {
+        return ((this->get()->op_notEqual(*this->get(),rhs.get())));
+    }
+    inline bool operator==(const ref<T> &rhs) {
+        return (this->get()->op_Equal(*this->get(),rhs.get()));
+    }
+
+    auto operator[](int index);
 };
 
-#endif // Refrence_Counter_H
+#endif // REFERENCECOUNT_H_CSQ4
